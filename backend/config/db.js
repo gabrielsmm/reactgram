351aa3fcd9f5f5466c595a5da414ff62f2372196
gabrielsmm <gabrielsmm251@gmail.com>
2023-02-16 20:15:50 -0300

    Conectando ao banco de dados

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const dbuser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async () => {
+
+    try {
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbuser}:${dbPassword}@cluster0.ndkp11n.mongodb.net/?retryWrites=true&w=majority`);
+        
+        console.log("Conectou ao banco!");
+        return dbConn;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+conn();
+
+module.exports = conn;
