22480e0b2e946a560ad1e4019b9402b1cde2735e
gabrielsmm <gabrielsmm251@gmail.com>
2023-02-21 23:01:26 -0300

    Buscando usuário por id

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const { register, login, getCurrentUser, update } = require("../controllers/UserController");
+const { register, login, getCurrentUser, update, getUserById } = require("../controllers/UserController");
 
 // Middlewares
 const validate = require("../middlewares/handleValidation");
@@ -15,5 +15,6 @@ router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
 router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update);
+router.get("/:id", getUserById);
 
 module.exports = router;
