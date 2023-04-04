6fde49bead9683f0f79c07203f2d9900310972ac
gabrielsmm <gabrielsmm251@gmail.com>
2023-04-03 21:35:08 -0300

    Implementação do Login

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,8 +1,48 @@
 import "./Auth.css";
 
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, reset } from "../../slices/authSlice";
+
+import Message from "../../components/Message";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      password
+    }
+
+    dispatch(login(user));
+  }
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
-    <div>Login</div>
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email} />
+        <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password} />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+      </form>
+      <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
+    </div>
   );
 };
 
--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -28,6 +28,19 @@ export const logout = createAsyncThunk("auth/logout", async () => {
     await authService.logout();
 });
 
+// Sign in
+export const login = createAsyncThunk("auth/login",
+    async(user, thunkAPI) => {
+        const data = await authService.login(user);
+
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0]);
+        }
+
+        return data;
+    }
+);
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -58,6 +71,21 @@ export const authSlice = createSlice({
             state.loading = false;
             state.success = true;
             state.error = null;
+            state.user = null;
+        })
+        .addCase(login.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(login.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
+        })
+        .addCase(login.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
             state.user = null;
         });
     }
